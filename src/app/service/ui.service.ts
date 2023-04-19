e37530f712d92ad1f431e1317b1ca2ae40dfcdfb
Maximilian Olzinger <mo@clouddna.at>
2023-04-19 18:45:50 +0200

    FP-8

--- src/app/service/ui.service.ts
+++ src/app/service/ui.service.ts
@@ -8,6 +8,25 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class UiService {
 
   private static BASE_URL: string = AppConfig.BASE_URL + "/store";
+  private static MEASUREMENT_URL: string = UiService.BASE_URL + "/measurement";
 
   constructor(private http:  HttpClient) { }
+
+  //Measurements ----
+
+  public getMeasurements(): Promise<any[] | undefined> {
+    const params = new HttpParams();
+
+    return this.http.get<any[]>(UiService.MEASUREMENT_URL, {
+      params
+    }).toPromise();
+  }
+
+  public createMeasurement(data: any): Promise<any | undefined> {
+    const params = new HttpParams();
+
+    return this.http.post<any>(`${UiService.MEASUREMENT_URL}`, data, {
+      params
+    }).toPromise();
+  }
 }
