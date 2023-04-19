3b624d7de1530e01f2da69edb1c75b91afbf4422
Maximilian Olzinger <mo@clouddna.at>
2023-04-19 18:46:53 +0200

    FP-9

--- src/app/service/ui.service.ts
+++ src/app/service/ui.service.ts
@@ -9,6 +9,7 @@ export class UiService {
 
   private static BASE_URL: string = AppConfig.BASE_URL + "/store";
   private static MEASUREMENT_URL: string = UiService.BASE_URL + "/measurement";
+  private static TELEMETRY_URL: string = UiService.BASE_URL + "/telemetry";
 
   constructor(private http:  HttpClient) { }
 
@@ -26,6 +27,24 @@ export class UiService {
     const params = new HttpParams();
 
     return this.http.post<any>(`${UiService.MEASUREMENT_URL}`, data, {
+      params
+    }).toPromise();
+  }
+
+  //Telemetry ----
+
+  public getTelemetries(): Promise<any[] | undefined> {
+    const params = new HttpParams();
+
+    return this.http.get<any[]>(UiService.TELEMETRY_URL, {
+      params
+    }).toPromise();
+  }
+
+  public createTelemetry(data: any): Promise<any | undefined> {
+    const params = new HttpParams();
+
+    return this.http.post<any>(`${UiService.TELEMETRY_URL}`, data, {
       params
     }).toPromise();
   }
