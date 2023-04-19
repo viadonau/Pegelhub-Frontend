1ab64b135890bb887f8c333bd19abba64675afd3
Maximilian Olzinger <mo@clouddna.at>
2023-04-19 20:03:59 +0200

    FP-55

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
