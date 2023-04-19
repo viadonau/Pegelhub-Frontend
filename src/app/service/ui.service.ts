d7350f42884fea668fe8d0bb13faf984cd8c669e
Maximilian Olzinger <mo@clouddna.at>
2023-04-19 18:39:13 +0200

    FP-7

--- src/app/service/ui.service.ts
+++ src/app/service/ui.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { AppConfig } from '../shared/Config';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
 
-  constructor() { }
+  private static BASE_URL: string = AppConfig.BASE_URL + "/store";
+
+  constructor(private http:  HttpClient) { }
 }
---
+++ src/app/shared/Config.ts
@@ -0,0 +1,8 @@
+import {environment} from '../../environments/environment';
+
+class AppConfig {
+  // available routes
+  public static readonly BASE_URL = environment.BASE_URL;
+}
+
+export {AppConfig};
