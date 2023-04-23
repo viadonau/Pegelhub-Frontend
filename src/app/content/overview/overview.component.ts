3721913af91e363a71b6a9330628f7974af9a4ef
Rene <rene.glavanovits@clouddna.at>
2023-04-23 10:16:26 +0200

    added navigation

--- src/app/content/overview/overview.component.ts
+++ src/app/content/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Supplier } from 'src/app/service/model/supplier.model';
 import { UiService } from 'src/app/service/ui.service';
 
@@ -11,11 +12,16 @@ export class OverviewComponent implements OnInit {
   supplier: Supplier[] = [];
 
   constructor(
-    private uiService: UiService
+    private uiService: UiService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.loadSupplier();
+  }
+
+  navigateToDetail(id: number): void {
+    this.router.navigate(['/', 'detail', id]);
   }
 
   private loadSupplier(): void {
