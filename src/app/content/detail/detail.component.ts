a674aaeaf2e86d27260c6e2cbe654d3acdf50c17
Rene <rene.glavanovits@clouddna.at>
2023-06-15 14:11:45 +0200

    minor changes

--- src/app/content/detail/detail.component.ts
+++ src/app/content/detail/detail.component.ts
@@ -217,7 +217,7 @@ export class DetailComponent implements OnInit, OnDestroy {
 
   private prepareMeasurementsForChart(arr: Measurement[]): any {
     return arr.map(entry => {
-      return [new Date(entry.timestamp).getTime() + new Date(entry.timestamp).getTimezoneOffset() * -60 * 1000, parseFloat((entry.fields as any).value)];
+      return [new Date(entry.timestamp).getTime() + new Date(entry.timestamp).getTimezoneOffset() * -60 * 1000, Math.round(parseFloat((entry.fields as any).value))];
     }).sort((a, b) => a[0] - b[0])
   }
 
