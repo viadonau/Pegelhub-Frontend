39613f87241625a6abb471a625bdd0a01da7a26b
Rene <rene.glavanovits@clouddna.at>
2023-06-19 10:29:36 +0200

    minor changes

--- src/app/service/auth.interceptor.ts
+++ src/app/service/auth.interceptor.ts
@@ -12,9 +12,9 @@ export class AuthInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return this.authService.authData.pipe(take(1), exhaustMap(authData => {
-            const apiKey = authData?.apiKey || 'O5p1A2NCmTgEX6snXO9mGJKaPp3pNaKhj6RAUK5glP1mOjh1Vm2M0OwPjooMc5fT';
+            const apiKey = authData?.apiKey;
 
-            const modifiedReq = apiKey == null ? req.clone() : req.clone({
+            const modifiedReq = apiKey == null || apiKey == '' ? req.clone() : req.clone({
                 params: req.params.set('apiKey', apiKey)
             });
 
