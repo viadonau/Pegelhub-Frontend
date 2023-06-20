b346e47853c009c7258ff173c6011983edac14b8
Maximilian Olzinger <52999477+molzi@users.noreply.github.com>
2023-06-20 13:51:59 +0200

    Update measurement.model.ts

--- src/app/service/model/measurement.model.ts
+++ src/app/service/model/measurement.model.ts
@@ -2,6 +2,7 @@ export interface Measurement {
     id: string;
     timestamp: string;
     fields: {[key: string]: string};
+    infos: {[key: string]: string};
     /* measurement: string;
     timestamp: string;
     fields: Map<string, number>;
