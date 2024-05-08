9fff52c77f4adde0935fb4be79e5a71debc88d9e
Julian Zankl <2210859018@fh-burgenland.at>
2024-05-08 09:27:22 +0200

    PGL-36: added remaining login comp tests

--- src/app/content/login/login.component.cy.ts
+++ src/app/content/login/login.component.cy.ts
@@ -5,25 +5,41 @@ describe('Login Component', () => {
     cy.mount(LoginComponent);
   })
 
-  it('should disable next button if no api-key has been entered', () => {
-    cy.button('Weiter').should('be.disabled');
-  })
-
-  it('should perform redirect to overview page after next button has been clicked and api-key is valid', () => {
-    cy.get('input').should('have.attr', 'placeholder', 'API Key').type('asdf');
-    cy.button('Weiter').click();
-    // TODO: check for redirect
-  })
-
-  it.only('should show an asterisk inside the api-key input field', () => {
+  it('should show an asterisk inside the api-key input field', () => {
     cy.get('#required-icon').should('be.visible');
   })
 
-  it('should show an appropriate help text if the api-key input does not correspond to the Base64 pattern', () => {
-    // TODO
+  it('should show an appropriate help-text if no api-key has been entered', () => {
+    cy.get('#api-key-help').should('contain.text', 'Die Eingabe eines gültigen API Keys ist notwendig');
   })
 
-  it('should show an appropriate help text if the api-key input is empty', () => {
-    // TODO
+  it('should show an appropriate help-text if the api-key input does not conform to the Base64 pattern', () => {
+    cy.get('input').should('have.attr', 'placeholder', 'API Key').type('a');
+    cy.get('#api-key-help').should('contain.text', 'Der eingegebene API Key entspricht nicht dem Base64-Format');
+  })
+
+  it('should not show an appropriate help-text if the api-key conforms to the Base64 format', () => {
+    cy.get('input').should('have.attr', 'placeholder', 'API Key').type('asdf');
+    cy.get('#api-key-help').should('not.be.visible');
+  })
+
+  it('should disable next-button if no api-key has been entered', () => {
+    cy.button('Weiter').should('be.disabled');
+  })
+
+  it('should disable next-button if the api-key input does not conform to the Base64 format', () => {
+    cy.get('input').should('have.attr', 'placeholder', 'API Key').type('a');
+    cy.button('Weiter').should('be.disabled');
+  })
+
+  it('should show a tooltip while hovering over the next-button if no api-key has been entered', () => {
+    cy.button('Weiter').trigger('mouseenter', {force: true});
+    cy.tooltip('Die Eingabe eines gültigen API Keys ist notwendig').should('be.visible');
+  })
+
+  it('should show a tooltip while hovering over the next-button if the api-key does not conform to the Base64 pattern', () => {
+    cy.get('input').should('have.attr', 'placeholder', 'API Key').type('a');
+    cy.button('Weiter').trigger('mouseenter', {force: true});
+    cy.tooltip('Der eingegebene API Key entspricht nicht dem Base64-Format').should('be.visible');
   })
 })
