d1fa9427e8dfce4b2c38d4915727a2809cc7f37b
Julian Zankl <2210859018@fh-burgenland.at>
2024-05-24 20:54:51 +0200

    build: updated Angular to v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import {APP_INITIALIZER, ApplicationConfig} from "@angular/core";
-import {provideRouter, withComponentInputBinding} from "@angular/router";
-import {routes} from "./app.routes";
-import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi} from "@angular/common/http";
-import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
-import {AuthInterceptor} from "./service/auth.interceptor";
-import {AuthService} from "./service/auth.service";
+import { APP_INITIALIZER, ApplicationConfig } from "@angular/core";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { routes } from "./app.routes";
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from "@angular/common/http";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { AuthInterceptor } from "./service/auth.interceptor";
+import { AuthService } from "./service/auth.service";
 
 function checkExistingApiKey(authService: AuthService) {
   return () => authService.initialize();
